b1cca6fa049137c11175a2a4653944be5b62aa50
jon-atto-bennett <jon.bennett@outlook.co.nz>
2017-05-10 21:25:41 +1200

    match truthiness

--- test.js
+++ test.js
@@ -16,3 +16,21 @@ test ('object assignment', () => {
 test('adding positive numbers is not zero', () => {
       expect(main.add(1, 2)).not.toBe(0)
   })
+
+  test('null', () => {
+  const n = null;
+  expect(n).toBeNull();
+  expect(n).toBeDefined();
+  expect(n).not.toBeUndefined();
+  expect(n).not.toBeTruthy();
+  expect(n).toBeFalsy();
+});
+
+test('zero', () => {
+  const z = 0;
+  expect(z).not.toBeNull();
+  expect(z).toBeDefined();
+  expect(z).not.toBeUndefined();
+  expect(z).not.toBeTruthy();
+  expect(z).toBeFalsy();
+});
