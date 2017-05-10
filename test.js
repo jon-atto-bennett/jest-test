e012c06e9271a5c3b8bb3bf291eb61b00f9f7640
jon-atto-bennett <jon.bennett@outlook.co.nz>
2017-05-10 18:32:25 +1200

    match multipy function

---
+++ test.js
@@ -0,0 +1,9 @@
+const main = require('./main.js')
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(main.sum(1, 2)).toBe(3)
+})
+
+test('multiply 2 * 3 to equal 6', () => {
+  expect(main.multiply(2, 3)).toBe(6)
+})
